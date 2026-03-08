19ee172a2a54d3517081726c075e986303607b31
TargetSplashed <1601166563@qq.com>
2026-03-08 14:46:23 +0800

    0.1.0 初始化项目

---
+++ code/gymrat/src/main/java/com/springfield/gymrat/GymratApplication.java
@@ -0,0 +1,13 @@
+package com.springfield.gymrat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymratApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymratApplication.class, args);
+    }
+
+}
---
+++ code/gymrat/src/test/java/com/springfield/gymrat/GymratApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springfield.gymrat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymratApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
