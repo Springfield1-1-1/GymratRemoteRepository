5b2ab42dc80e0572fed3b2d63f2c86c27259f6d3
TargetSplashed <1601166563@qq.com>
2026-03-09 12:45:51 +0800

    0.1.2 修复了版本兼容问题导致项目无法运行的bug

--- code/gymrat/src/main/java/com/springfield/gymrat/GymratApplication.java
+++ code/gymrat/src/main/java/com/springfield/gymrat/GymratApplication.java
@@ -1,9 +1,11 @@
 package com.springfield.gymrat;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.springfield.gymrat.mapper")
 public class GymratApplication {
 
     public static void main(String[] args) {
@@ -11,3 +13,5 @@ public class GymratApplication {
     }
 
 }
+//TODO: 解决版本兼容问题
+//TODO: 检查并接口测试登录功能，成功后提交至仓库
