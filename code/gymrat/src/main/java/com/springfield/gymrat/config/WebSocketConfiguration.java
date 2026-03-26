2d33246baecdcb8a59503065159f77dfd777ff14
TargetSplashed <1601166563@qq.com>
2026-03-26 18:27:35 +0800

    1.0.1 将@Configuration注解替换为@ConditionalOnWebApplication，确保WebSocket配置仅在Web应用环境中生效

--- code/gymrat/src/main/java/com/springfield/gymrat/config/WebSocketConfiguration.java
+++ code/gymrat/src/main/java/com/springfield/gymrat/config/WebSocketConfiguration.java
@@ -1,11 +1,12 @@
 package com.springfield.gymrat.config;
 
 import com.springfield.gymrat.websocket.WebSocketServer;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
-@Configuration
+@ConditionalOnWebApplication
 public class WebSocketConfiguration {
 
     @Bean
